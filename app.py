b654c22606b0f3ac4981b3390abdfbd8037e1d62
hassancodes <59165844+hassancodes@users.noreply.github.com>
2022-01-30 08:33:16 -0500

    adding text message option

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask
-from flask import render_template
-app = Flask(__name__)
+from flask import render_template,redirect,request,session
+import json
+import requests
+import os
 
+app = Flask(__name__)
+app.secret_key = "botsystematic"
+
+
+dictionary = {}
 @app.route("/")
 def login():
     return render_template("login.html")
@@ -9,9 +16,75 @@ def login():
 
 @app.route("/enterClass")
 def enterClass():
-    return render_template("class.html")
+    if "username" in session:
+        return render_template("class.html", dictionary=dictionary)
+    else:
+        return redirect("/")
+
+
+@app.route("/handlecred", methods=['POST'])
+def handlecred():
+    if request.method == "POST":
+        studid = request.form["studentid"]
+        pw = request.form["pass"]
+        with open("data.json", "r") as pd:
+            a = pd.read()
+            rawdata = json.loads(a)
+            for users in rawdata:
+                if rawdata[users]["username"] == studid and rawdata[users]["password"] == pw:
+                    session["username"] = studid
+                    session["password"] = pw
+                    return redirect("/enterClass")
+            else:
+                return redirect("/")
+
+
+# function for sending textmessages
+@app.route("/sendmessages", methods=["POST"])
+def sendmessages():
+    text = request.form["text"]
+    print(text)
+    obj = {"text": text,
+    "username": session["username"],
+    "secret": "iamdope"}
+
+    req = requests.post("http://127.0.0.1:8000/chat", data=obj)
+    text = req.text
+    dat = json.loads(text)
+    dictionary.update({dat["username"] : dat["text"]})
+    print(dictionary)
+
+    if os.path.getsize('text.json') == 0:
+        with open("text.json", "r+") as file:
+            json.dump(dictionary, file)
+
+    elif os.path.getsize('text.json') != 0:
+        with open("text.json", "r+") as flo:
+            data = flo.read()
+            print("DATA________________________",data)
+            flo.seek(0)
+            json.dump(data, flo)
+
+    with open("text.json", "r") as fl:
+        data = json.loads(fl.read())
+        print(data)
+
+
+    return redirect("/enterClass")
 
 
 
 
-app.run(debug=True)
+    # return redirect("/enterClass" , dictionary=dictionary)
+
+
+
+
+
+@app.route("/createaccount")
+def createaccount():
+    return "LOL"
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
---
+++ test.py
@@ -0,0 +1,48 @@
+import cv2
+import glob
+def getcordlist(image):
+    method = cv2.TM_SQDIFF_NORMED
+    small = image
+    large = cv2.imread('bg.png')
+    result = cv2.matchTemplate(small, large, method)
+    # We want the minimum squared difference
+    mn,_,mnLoc,_ = cv2.minMaxLoc(result)
+    # Draw the rectangle:
+    # Extract the coordinates of our best match
+    xaxis,yaxis = mnLoc
+    # Step 2: Get the size of the template. This is the same size as the match.
+    trows,tcols = small.shape[:2]
+    # getting the mid point of the rectangle that we just create from the image matched area.
+    point = (xaxis+int(tcols/2),yaxis+int(trows/2))
+    return point
+
+# function ends here
+
+
+def getimages():
+    path = glob.glob("cordimages/*.png")
+    # cv_image will store all the images
+    cv_img = []
+    for img in path:
+        n = cv2.imread(img)
+        cv_img.append(n)
+
+    # cordinates for storing the points
+    cordinates = []
+    for i in cv_img:
+        p = getcordlist(i)
+        cordinates.append(p)
+    large = cv2.imread('bg.png')
+    print(cordinates)
+    # [(718, 431), (839, 354), (298, 293), (447, 384), (547, 440)]
+    for point in cordinates:
+        cv2.circle(large,tuple(point),1,(255,0,0), thickness=10)
+    cv2.imshow("output",large)
+    cv2.waitKey(0)
+# getimages()
+
+
+
+import requests
+a = requests.get("https://api.muhammadhassa12.repl.co")
+print(a.text)
